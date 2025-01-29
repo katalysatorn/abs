6979b0b68e9d53c0bd9e5218dff04c1b14986e2a
katalysatorn. <me@katalysatorn.xyz>
2025-01-29 18:21:47 +1300

    Got to open a file - changed exampledir to abs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use serde::Serialize;
+use serde_json::{Deserializer, Serializer};
+use std::{fs::File, io::BufReader};
+use tracing::{error, info};
+
+const CONFIGFILEPATHS: [&str; 3] = ["./abs", "./exampledir/abs.json", "~/.config/abs/abs.json"];
+
+fn open_config() -> Result<(), ()> {
+    for validpath in CONFIGFILEPATHS {
+        match File::open(validpath) {
+            Ok(f) => {
+                let mut r = BufReader::new(f);
+                info!("Found config file {}", validpath);
+                serde_json::Deserializer::from_reader(r);
+                break;
+            }
+
+            Err(err) => {
+                error!("Failed reading config file\n\n{}", err);
+            }
+        }
+    }
+
+    return Err(());
+}
+
 fn main() {
-    println!("Hello, world!");
+    tracing_subscriber::fmt::init();
+
+    let mut absconfig = open_config();
+    assert!(absconfig.is_ok());
+    let mut absconfig = absconfig.unwrap();
 }
