875a44b6ea26b6d4443736bf149aafed0d2342da
katalysatorn. <me@katalysatorn.xyz>
2025-02-09 13:55:03 +1300

    Added functionality for building AUR pkgs

--- abs.sh
+++ abs.sh
@@ -2,9 +2,6 @@
 
 source ./config/abs.conf
 
-ROOTDIR=$(pwd)
-echo $ROOTDIR
-
 # Setup TMPDIR
 if ! [[ -v "$TMPDIR" ]]; then
 	TMPDIR=/tmp/abs
@@ -19,7 +16,36 @@ cd $PUBDIR
 # 	rsync -rlptHvh --progress --safe-links --delete-delay --delay-updates $mirror $(basename $mirror)
 # done
 
-# Build custom-packages
-for pkg in $SRCDIR/custom/*; do
-	echo "$pkg" >> $TMPDIR/built
+buildpkg() {
+	local repo=$1
+	makepkg $MAKEPKGFLAGS
+	mv *.pkg.tar.zst $REPODIR/$repo
+	mv *.pkg.tar.zst.sig $REPODIR/$repo
+
+	# Add built packages to repo
+	cd $REPODIR/$repo
+	repo-add --sign --include-sigs -nR $repo.db.tar.xz *.pkg.tar.zst
+}
+
+for repo in "${REPO_NAMES[@]}"; do
+	MAKEPKGFILE=/etc/makepkg.conf
+	if [[ -f $CONFIGDIR/$repo-makepkg.conf ]]; then
+		MAKEPKGFILE="$CONFIGDIR/$repo-makepkg.conf"
+	fi
+	
+	# Build custom-packages
+	for pkg in $SRCDIR/custom/*; do
+		cd $pkg
+		buildpkg $repo
+	done
+
+	cd $TMPDIR
+	# Build AUR packages
+	for pkg in "${PACKAGELIST}"; do
+		repoctl down -d $SRCDIR/aur -o build-order $pkg
+		for build in $(cat $TMPDIR/build-order); do
+			cd $SRCDIR/aur/$build
+			buildpkg $repo
+		done
+	done
 done
