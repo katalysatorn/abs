adf68c5efee97f4d2c06511b7c190b3bcf3aa4b3
katalysatorn. <me@katalysatorn.xyz>
2025-02-09 13:05:37 +1300

    Add RSYNC mirrors - change config a bit and start writing to build custom packages

--- abs.sh
+++ abs.sh
@@ -1,1 +1,25 @@
 #!/bin/bash
+
+source ./config/abs.conf
+
+ROOTDIR=$(pwd)
+echo $ROOTDIR
+
+# Setup TMPDIR
+if ! [[ -n "$TMPDIR" ]]; then
+	TMPDIR=/tmp/abs
+fi
+echo "" > $TMPDIR/built
+
+# Rsync
+cd $PUBDIR
+
+# for mirror in "${RSYNCMIRRORS[@]}"
+# do
+# 	rsync -rlptHvh --progress --safe-links --delete-delay --delay-updates $mirror $(basename $mirror)
+# done
+
+# Build custom-packages
+for pkg in $SRCDIR/custom/*; do
+	echo "$pkg" >> $TMPDIR/built
+done
